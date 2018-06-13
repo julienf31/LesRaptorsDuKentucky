95c9a44c8ff8f12e09601dae66b300d18596d757
julienf31 <julien.fournier@ynov.com>
2018-06-13 22:19:54 +0200

    🦖  Ampelosaurus

--- src/app/home/activities/activities-excerpt/activities-excerpt.component.ts
+++ src/app/home/activities/activities-excerpt/activities-excerpt.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-activities-excerpt',
   templateUrl: './activities-excerpt.component.html',
   styleUrls: ['./activities-excerpt.component.scss'],
 })
+
 export class ActivitiesExcerptComponent implements OnInit {
+
+  @Input() name: string;
+  @Input() text: string;
 
   constructor() { }
 
